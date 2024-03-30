58a11e797307002acccd647f42142da9b411bbd0
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-03-30 18:35:05 +0700

    feature: append actions and read from file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{collections::HashMap, error};
+use std::str::FromStr;
+use std::{collections::HashMap, io::Read};
 
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action!");
@@ -6,9 +7,11 @@ fn main() {
 
     println!("Action: {:?} - Item: {:?}", action, item);
 
-    let mut todo = Todo {
-        map: HashMap::new(),
-    };
+    // let mut todo = Todo {
+    //     map: HashMap::new(),
+    // };
+
+    let mut todo = Todo::new_loop().expect("Initialisation of db failed");
 
     if action == "add" {
         todo.insert(item);
@@ -25,6 +28,53 @@ struct Todo {
 }
 
 impl Todo {
+    fn new() -> Result<Todo, std::io::Error> {
+        let mut file_options = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.txt")?;
+
+        let mut content = String::new();
+
+        file_options.read_to_string(&mut content);
+
+        let map: HashMap<String, bool> = content
+            .lines()
+            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
+            .map(|v| (v[0], v[1]))
+            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
+            .collect();
+        Ok(Todo { map })
+    }
+
+    fn new_loop() -> Result<Todo, std::io::Error> {
+        // open the db file
+        let mut f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.txt")?;
+        // read its content into a new string
+        let mut content = String::new();
+        f.read_to_string(&mut content)?;
+
+        // allocate an empty HashMap
+        let mut map = HashMap::new();
+
+        // loop over each lines of the file
+        for entries in content.lines() {
+            // split and bind values
+            let mut values = entries.split('\t');
+            let key = values.next().expect("No Key");
+            let val = values.next().expect("No Value");
+            // insert them into HashMap
+            map.insert(String::from(key), bool::from_str(val).unwrap());
+        }
+        // Return Ok
+        Ok(Todo { map })
+    }
+
     fn insert(&mut self, key: String) {
         self.map.insert(key, true);
     }
